9748e95d9aeef5574e4631446cc279f101d81b98
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-12-03 09:08:05 -0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by kenjimaeda on 03/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
